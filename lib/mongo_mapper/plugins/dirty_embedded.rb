e4d7e74572f0f448a60af86aaa31f28176090e7a
Tomas Celizna <tomas.celizna@gmail.com>
2011-02-18 23:10:37 +0100

    comments

--- lib/mongo_mapper/plugins/dirty_embedded.rb
+++ lib/mongo_mapper/plugins/dirty_embedded.rb
@@ -17,13 +17,13 @@ module MongoMapper
         
         # don't understand why this is need
         # remove changed_attribute if it has the same value like the current attribute value
-        
         def changed_attributes
           super.delete_if{|k,v| v == __send__(k)}
         end
                 
         protected
         
+        # overwrite default, to also include all attributes
         def attribute_method?(attr)
           super || attributes.keys.include?(attr)
         end
